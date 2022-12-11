2968a752041f59143c399f07d321d770b83f1312
Filip Chorbeski <86695898+FilipChorbeski@users.noreply.github.com>
2022-12-11 14:47:07 +0100

    view study spot details and pass id
    Co-Authored-By: SazdovaEkaterina <74919977+SazdovaEkaterina@users.noreply.github.com>

--- Домашна 2/tech prototype/StudySpot/src/main/java/ukim/finki/dians/studyspot/web/controller/SpotsNearYouController.java
+++ Домашна 2/tech prototype/StudySpot/src/main/java/ukim/finki/dians/studyspot/web/controller/SpotsNearYouController.java
@@ -1,8 +1,10 @@
 package ukim.finki.dians.studyspot.web.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import ukim.finki.dians.studyspot.service.SpotService;
@@ -21,6 +23,12 @@ public class SpotsNearYouController {
     public String getStudySpotsNearYou(HttpSession httpSession) {
         httpSession.setAttribute("spots",this.spotService.listAllSpots());
         return "spots_near_you";
+    }
+
+    @PostMapping
+    public String buttonSubmit(HttpServletRequest request, HttpSession httpSession) {
+        httpSession.setAttribute("spot_id", request.getParameter("details_button"));
+        return "redirect:/study-spot";
     }
 
 }
--- Домашна 2/tech prototype/StudySpot/src/main/java/ukim/finki/dians/studyspot/web/controller/SpotsOfTypeController.java
+++ Домашна 2/tech prototype/StudySpot/src/main/java/ukim/finki/dians/studyspot/web/controller/SpotsOfTypeController.java
@@ -1,9 +1,11 @@
 package ukim.finki.dians.studyspot.web.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -13,7 +15,14 @@ public class SpotsOfTypeController {
 
     @GetMapping
     public String getStudySpotsOfType()  {
+
         return "spots_of_type";
+    }
+
+    @PostMapping
+    public String buttonSubmit(HttpServletRequest request, HttpSession httpSession) {
+        httpSession.setAttribute("spot_id", request.getParameter("details_button"));
+        return "redirect:/study-spot";
     }
 
 }
