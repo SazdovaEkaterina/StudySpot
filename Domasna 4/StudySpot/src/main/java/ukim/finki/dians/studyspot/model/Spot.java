d8d58d2061351589cf25b674450a1e16a7515ee1
SazdovaEkaterina <sazdovaekaterina@gmail.com>
2023-01-28 20:06:09 +0100

    rename variable
    all variables should use camelcase for consistency

--- Domasna 4/StudySpot/src/main/java/ukim/finki/dians/studyspot/model/Spot.java
+++ Domasna 4/StudySpot/src/main/java/ukim/finki/dians/studyspot/model/Spot.java
@@ -11,15 +11,15 @@ public class Spot {
     private String name;
     private String location;
     private String amenity;
-    private String opening_hours;
+    private String openingHours;
 
-    public Spot(long id, double latitude, double longitude, String name, String location, String amenity, String opening_hours) {
+    public Spot(long id, double latitude, double longitude, String name, String location, String amenity, String openingHours) {
         this.id = id;
         this.latitude = latitude;
         this.longitude = longitude;
         this.name = name;
         this.location = location;
         this.amenity = amenity;
-        this.opening_hours = opening_hours;
+        this.openingHours = openingHours;
     }
 }
